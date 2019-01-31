3c35cf93268d9eddb2991a2e39e5dd7022aa80c1
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2019-01-30 18:38:38 -0600

    remove duplicate function

--- 01-basics/basics.cpp
+++ 01-basics/basics.cpp
@@ -10,10 +10,3 @@ int multiply_numbers(int num1, int num2)
 	return num1 * num2;
 }
 
-//write code for multiply_numbers function here
-
-
-int multiply_numbers(int num1, int num2);
-{
-	return num1* num2;
-}
